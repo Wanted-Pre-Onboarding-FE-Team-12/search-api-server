c2596e8c80a453a59144622d003f872084a91dfe
KiHoon Roh <started677@gmail.com>
2022-09-28 13:41:51 +0900

    feat: api 서버 구현

---
+++ server.js
@@ -0,0 +1,16 @@
+const data = require('./db.json');
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 4000;
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+
+server.listen(port, () => {
+  console.log('Server is Running');
+});
